6dab5b13e2a0b510b4e2274b850d6cc6cedb7283
zakkdruzer <zakkdruzer@gmail.com>
2026-08-10 22:56:23 -0300

    feat: estructura base lista

---
+++ cypress.config.js
@@ -0,0 +1,20 @@
+// cypress.config.js
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  e2e: {
+    // URL base de tu app local
+    baseUrl: 'http://localhost:5173',
+    // Dónde están tus specs
+    specPattern: 'cypress/e2e/**/*.cy.js',
+    // Archivo de soporte global
+    supportFile: 'cypress/support/e2e.js',
+    // Tamaño de ventana para pruebas
+    viewportWidth: 1280,
+    viewportHeight: 800,
+    // Tiempo máximo para comandos
+    defaultCommandTimeout: 4000,
+    // Desactivar video para que sea más rápido
+    video: false
+  }
+})
